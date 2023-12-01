258e0fb8fcd003167dd8a5d09540658aca6dbb3e
arshkhatwani <arsh.khatwani@gmail.com>
2023-12-02 00:35:51 +0530

    Adds findFirst

--- script.ts
+++ script.ts
@@ -2,12 +2,19 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-    const user = await prisma.user.findUnique({
+    // const user = await prisma.user.findUnique({
+    //     where: {
+    //         age_name: {
+    //             name: "Kyle",
+    //             age: 24,
+    //         },
+    //     },
+    // });
+    // console.log(user);
+
+    const user = await prisma.user.findFirst({
         where: {
-            age_name: {
-                name: "Kyle",
-                age: 24,
-            },
+            name: "Sally",
         },
     });
     console.log(user);
